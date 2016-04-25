33f97fe562202b530b1fc7802981ae2d3f103aaf
Oleksii Prudkyi <Oleksii.Prudkyi@gmail.com>
2016-04-25 07:56:24 +0300

    implements tests

---
+++ test/tests/acceptance/AcceptanceTestCest.php
@@ -0,0 +1,105 @@
+<?php
+
+
+class AcceptanceTestCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+    }
+	
+	private $beforeAllSmall = <<<'EOF'
+Starting : echo "Before All"
+Before All
+EOF;
+
+	private $beforeAll = <<<'EOF'
+Starting : echo "Before All"
+Before All
+Starting : Description of command
+Before All with Description
+Starting : echo "Before All single line"
+Before All single line
+Starting : Before All with Params
+Before All Params
+Starting : BeforeAll. fail on run but ignore errors
+Command result code : 1
+EOF;
+	
+	
+	private $beforeAnySuite = <<<'EOF'
+Starting : echo "Before any suite"
+Before any suite
+EOF;
+	
+	private $afterAnySuite = <<<'EOF'
+Starting : echo "After any suite"
+After any suite
+EOF;
+	
+	private $afterAll = <<<'EOF'
+Starting : echo "After All"
+After All
+EOF;
+	
+	private $beforeAcceptanceSuite = <<<'EOF'
+Starting : echo "Before acceptance suite"
+Before acceptance suite
+EOF;
+	
+	private $afterAcceptanceSuite = <<<'EOF'
+Starting : echo "After acceptance suite"
+After acceptance suite
+EOF;
+
+    public function withoutSuitedCommandsTest(AcceptanceTester $I)
+    {
+		chdir('../sample/');
+		$I->runShellCommand('./vendor/bin/codecept run functional --no-colors');
+		$I->seeInShellOutput("Functional Tests");
+		$I->dontSeeInShellOutput("Acceptance Tests");
+		$I->seeInShellOutput($this->beforeAllSmall);
+		$I->seeInShellOutput($this->beforeAll);
+		$I->seeInShellOutput($this->beforeAnySuite);
+		$I->seeInShellOutput($this->afterAnySuite);
+		$I->seeInShellOutput($this->afterAll);
+		$I->dontSeeInShellOutput($this->beforeAcceptanceSuite);
+		$I->dontSeeInShellOutput($this->afterAcceptanceSuite);
+		chdir('../test/');
+    }
+
+    public function withSuitedCommandsTest(AcceptanceTester $I)
+    {
+		chdir('../sample/');
+		$I->runShellCommand('./vendor/bin/codecept run acceptance --no-colors');
+		$I->dontSeeInShellOutput("Functional Tests");
+		$I->seeInShellOutput("Acceptance Tests");
+		$I->seeInShellOutput($this->beforeAllSmall);
+		$I->seeInShellOutput($this->beforeAll);
+		$I->seeInShellOutput($this->beforeAnySuite);
+		$I->seeInShellOutput($this->afterAnySuite);
+		$I->seeInShellOutput($this->afterAll);
+		$I->seeInShellOutput($this->beforeAcceptanceSuite);
+		$I->seeInShellOutput($this->afterAcceptanceSuite);
+		chdir('../test/');
+    }
+
+    public function fullCommandsTest(AcceptanceTester $I)
+    {
+		chdir('../sample/');
+		$I->runShellCommand('./vendor/bin/codecept run --no-colors');
+		$I->seeInShellOutput("Functional Tests");
+		$I->seeInShellOutput("Acceptance Tests");
+		$I->seeInShellOutput($this->beforeAllSmall);
+		$I->seeInShellOutput($this->beforeAll);
+		$I->seeInShellOutput($this->beforeAnySuite);
+		$I->seeInShellOutput($this->afterAnySuite);
+		$I->seeInShellOutput($this->afterAll);
+		$I->seeInShellOutput($this->beforeAcceptanceSuite);
+		$I->seeInShellOutput($this->afterAcceptanceSuite);
+		chdir('../test/');
+    }
+}
